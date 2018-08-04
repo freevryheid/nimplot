c53d5d42db35f1b63ed4ddd9e08b7e7468fab1e1
Andre Smit <freevryheid@gmail.com>
2018-08-04 09:43:30 -0500

    update

--- plot.nim
+++ plot.nim
@@ -3,51 +3,57 @@ import
   , random
   , cairo
 
-const
-  CHART_WIDTH = 600
-  CHART_HEIGHT = 400
-  DRAW_AREA = 0.8
-
 type
   CColor = tuple
     r,g,b: cdouble
-  #ColSeq = seq[CColor]
+  ColorTable = array[6,CColor]
   Size = tuple
     w, h: cint
-  #Legend = object
-  #  text: Font
-  LineType = enum
+  Font = object
+  Legend = object
+  #  text: string
+  #  font: Font
+  #  show: bool
+  LineType* = enum
     None, Solid #,Gradient
-  Line = ref object
-    ltype: LineType
-    color: CColor
-    thick: float
-  #Marker = object
-  DataSet* = ref object
-    #legend*: Legend
-    x*: seq[float]
+  Line* = object
+    ltype*: LineType
+    color*: CColor
+    thick*: float
+  Marker = object
+  DataSet* = object
+    chart: Chart
+    legend*: Legend
+    x*: seq[float]  # primary axis
     y*: seq[float]
-    #x1*: seq[float]
-    #y1*: seq[float]
-    xmin, xmax, ymin, ymax: float
-    #xAxis: Axis
-    #yAxis: Axis
-    #y0Axis: Axis
-    #y1Axis: Axis
-    line: Line
-    #marker: Marker
+    u*: seq[float]  # secondary axis
+    v*: seq[float]
+    xmin, xmax, ymin, ymax: float  # extrema 1
+    umin, umax, vmin, vmax: float  # extrema 2
+    xAxis*: Axis
+    yAxis*: Axis
+    uAxis*: Axis
+    vAxis*: Axis
+    line*: Line
+    marker*: Marker
     #markerType: MarkerType
     #markerColor*: CColor
     #markerSize*: float
-  DataSets* = seq[DataSet]
+  DataSets = seq[DataSet]
   ChartType = enum
     scatter
-  Chart* = object of RootObj
+  Title = object
+    text: string
+    color: CColor
+    h: float  # height
+  Chart = object of RootObj
     ctype: ChartType
-    size*: Size  # chart size
+    size: Size  # chart size
+    nsets: int  # number of datasets
     series*: DataSets
-    #colSeq: ColSeq
-    drawArea*: float # as fraction of size
+    colorTable: ColorTable
+    frame: Line
+    title: Title
   Scatter* = object of Chart
 
 proc ccolor(c: Color): CColor =
@@ -56,38 +62,55 @@ proc ccolor(c: Color): CColor =
   result.g = cdouble(x.g/255)
   result.b = cdouble(x.b/255)
 
-const colortable = [
-  ccolor(colRed),
-  ccolor(colGreen),
-  ccolor(colBlue)]
+const
+  BLACK = ccolor(colBlack)
+  CHART_WIDTH = 600
+  CHART_HEIGHT = 400
+  TITLE_HEIGHT = 100
+  TITLE_COLOR = BLACK
+  FRAME_LTYPE = Solid
+  FRAME_THICK = 5
+  FRAME_COLOR = BLACK
+  LINE_LTYPE = Solid
+  LINE_THICK = 1
+  COLORTABLE01 = [
+    ccolor(colRed),
+    ccolor(colGreen),
+    ccolor(colBlue),
+    ccolor(colYellow),
+    ccolor(colCyan),
+    ccolor(colMagenta)]
+
+proc newDataSet*(chart: var Chart): DataSet =
+  result.chart = chart
+  # lines
+  result.line.ltype = LINE_LTYPE
+  result.line.thick = LINE_THICK
+  result.line.color = chart.colorTable[chart.nsets]
+  # markers
+  # axis
+  # legend
+  inc chart.nsets
 
 proc newScatter*(): Scatter =
+  # chart options
   result.ctype = scatter
   var size:Size
   size.w = CHART_WIDTH
   size.h = CHART_HEIGHT
   result.size = size
-  result.drawArea = DRAW_AREA
   result.series = @[]
+  result.colorTable = COLORTABLE01
+  # chart frame
+  result.frame.ltype = FRAME_LTYPE
+  result.frame.thick = FRAME_THICK
+  result.frame.color = FRAME_COLOR
+  # chart title
+  result.title.h = TITLE_HEIGHT
+  result.title.color = TITLE_COLOR
+  # chart
 
-proc setWidth*(chart: var Chart; w: cint) =
-  chart.size.w = w
-
-proc getWidth*(chart: Chart): cint =
-  result = chart.size.w
-
-proc setHeight*(chart: var Chart; h: cint) =
-  chart.size.h = h
-
-proc getHeight*(chart: Chart): cint =
-  result = chart.size.h
-
-proc setSize*(chart: var Chart; size: Size) =
-  chart.size = size
-
-proc getSize*(chart: Chart): Size =
-  result = chart.size
-
+# TODO: Update to check secondary axis as well
 proc extrema(series: var DataSets) =
   var smin,smax,tmin,tmax: float
   for i, s in series.mpairs:
@@ -110,37 +133,25 @@ proc extrema(series: var DataSets) =
       if tmax > s.ymax:
         s.ymax = tmax
 
-proc addlines(series: var DataSets) =
-  for i, s in series.mpairs:
-    if isNil(s.line):
-      s.line.ltype = Solid
-      s.line.thick = 0.5
-      s.line.color = colortable[i]  # TODO
-    else:
-
-    #if s.line.color == nil: s.line.color = colortable[i]
-    #if s.line.ltype == nil: s.line.ltype = Solid
-
-proc examine(chart: var Chart) =
-  if len(chart.series) > 0:
-    chart.series.extrema()
-    chart.series.addlines()
-
 proc plot*(chart: var Chart): ptr cairo_t =
-  chart.examine()
+  extrema(chart.series)
   let sf = image_surface_create(FORMAT_ARGB32, chart.size.w, chart.size.h)
   defer: surface_destroy(sf)
   result = create(sf)
   # lines
   for s in chart.series:
+    if s.line.ltype == None:
+      continue
     result.set_source_rgb(s.line.color.r, s.line.color.g, s.line.color.b)
     result.set_line_width(s.line.thick)
+    # TODO assert len(x) == len(y)
     for i in 0..len(s.x)-2:
-      result.move_to(chart.size.w.float*(s.x[i]-s.xmin)/(s.xmax-s.xmin),chart.size.h.float*(s.y[i]-s.ymin)/(s.ymax-s.ymin))
-      result.line_to(chart.size.w.float*(s.x[i+1]-s.xmin)/(s.xmax-s.xmin),chart.size.h.float*(s.y[i+1]-s.ymin)/(s.ymax-s.ymin))
+      result.move_to(chart.size.w.float*(s.x[i]-s.xmin)/(s.xmax-s.xmin), chart.size.h.float - chart.title.h - chart.size.h.float*(s.y[i]-s.ymin)/(s.ymax-s.ymin))
+      result.line_to(chart.size.w.float*(s.x[i+1]-s.xmin)/(s.xmax-s.xmin), chart.size.h.float - chart.title.h - chart.size.h.float*(s.y[i+1]-s.ymin)/(s.ymax-s.ymin))
       result.stroke()
   # chart frame
-  result.set_source_rgb(0, 0, 0)
+  result.set_source_rgb(chart.frame.color.r, chart.frame.color.g, chart.frame.color.b)
+  result.set_line_width(chart.frame.thick)
   result.rectangle(0, 0, chart.size.w.float, chart.size.h.float)
   result.stroke()
 
--- test.nim
+++ test.nim
@@ -7,19 +7,22 @@ import
 proc main() =
   var
     scatter = newScatter()
-    ds1, ds2, ds3: DataSet
+    ds1 = scatter.newDataSet()
+    ds2 = scatter.newDataSet()
+    ds3 = scatter.newDataSet()
     x, y1, y2, y3: seq[float]
 
   x = toSeq(0..99).map(proc(z: int): float = float(z))
   ds1.x = x
   ds2.x = x
   ds3.x = x
-  y1 = x.map(proc(z: float): float = z*rand(100.0))
-  y2 = x.map(proc(z: float): float = z*rand(100.0))
-  y3 = x.map(proc(z: float): float = z*rand(100.0))
+  y1 = x.map(proc(z: float): float = rand(25.0))
+  y2 = x.map(proc(z: float): float = rand(50.0))
+  y3 = x.map(proc(z: float): float = rand(100.0))
   ds1.y = y1
   ds2.y = y2
   ds3.y = y3
+
   scatter.series.add(ds1)
   scatter.series.add(ds2)
   scatter.series.add(ds3)
