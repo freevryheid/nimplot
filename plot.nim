4452da84b6c09c2c4e8dd026583fe53b81bb5c70
Andre Smit <freevryheid@gmail.com>
2018-08-13 08:00:11 -0500

    add title

--- plot.nim
+++ plot.nim
@@ -16,7 +16,7 @@ proc ccolor(c: Color): CColor =
 type
   ColorTable = array[6,CColor]
   Size = tuple
-    w, h: cint
+    w, h: float
   Font = object
     name: string
     slant: font_slant_t    # TODO: change this
@@ -59,39 +59,40 @@ type
 #    marker*: Marker
   DataSets = seq[DataSet]
   ChartType = enum
-    scatter
+    SCATTER
   Title = object
     font: Font
     text: string
     loc: Loc  # location
+    size: Size  # w, h
   Chart = object of RootObj
     ctype: ChartType
     size: Size  # chart size
     nsets: int  # number of datasets
     series*: DataSets
     colorTable: ColorTable
-    gap: int  # between stuff
+    gap: float  # between stuff
     frame: Line  # chart frame
     title: Title
-  Scatter* = object of Chart
+  ScatterChart* = object of Chart
 
 const
   BLACK = ccolor(colBlack)
-  CHART_WIDTH = 600
-  CHART_HEIGHT = 400
-  CHART_GAP = 10  # Applied through between blocks
-  TITLE_FONT_NAME = "serif"
+  CHART_WIDTH = 600.0
+  CHART_HEIGHT = 400.0
+  CHART_GAP = 10.0  # Applied through between blocks
+  TITLE_FONT_NAME = "sans"
   TITLE_FONT_SLANT = FONT_SLANT_NORMAL
   TITLE_FONT_WEIGHT = FONT_WEIGHT_BOLD
-  TITLE_FONT_SIZE = 12
+  TITLE_FONT_SIZE = 30.0
   TITLE_FONT_FG = BLACK  # foreground color
   TITLE_LOC = NORTH
   TITLE_TEXT = ""
-  FRAME_LTYPE = SOLID
-  FRAME_THICK = 5
-  FRAME_COLOR = BLACK
+  CHART_FRAME_LTYPE = SOLID
+  CHART_FRAME_THICK = 5.0
+  CHART_FRAME_COLOR = BLACK
   LINE_LTYPE = SOLID
-  LINE_THICK = 1
+  LINE_THICK = 1.0
   COLORTABLE01 = [
     ccolor(colRed),
     ccolor(colGreen),
@@ -111,9 +112,9 @@ proc newDataSet*(chart: var Chart): DataSet =
   # legend
   inc chart.nsets
 
-proc newScatter*(): Scatter =
+proc newScatter*(): ScatterChart =
   # chart options
-  result.ctype = scatter
+  result.ctype = SCATTER
   var size: Size
   var titleFont: Font
   size.w = CHART_WIDTH
@@ -123,9 +124,9 @@ proc newScatter*(): Scatter =
   result.series = @[]
   result.colorTable = COLORTABLE01
   # chart frame
-  result.frame.ltype = FRAME_LTYPE
-  result.frame.thick = FRAME_THICK
-  result.frame.color = FRAME_COLOR
+  result.frame.ltype = CHART_FRAME_LTYPE
+  result.frame.thick = CHART_FRAME_THICK
+  result.frame.color = CHART_FRAME_COLOR
   # chart title
   titleFont.fg = TITLE_FONT_FG
   titleFont.name = TITLE_FONT_NAME
@@ -135,7 +136,6 @@ proc newScatter*(): Scatter =
   result.title.font = titleFont
   result.title.loc = TITLE_LOC
   result.title.text = TITLE_TEXT
-
 
 # TODO: Update to check secondary axis as well
 proc extrema(series: var DataSets) =
@@ -161,11 +161,17 @@ proc extrema(series: var DataSets) =
         s.ymax = tmax
 
 proc plot*(chart: var Chart): ptr cairo_t =
-  var te: ptr text_extents_t
-  var xcoord, ycoord: int
-  let sf = image_surface_create(FORMAT_ARGB32, chart.size.w, chart.size.h)
+  var
+    te: text_extents_t
+    #cfo : ptr font_options_t
+  #cfo = font_options_create()
+  #echo repr cfo
+  #cfo.font_options_set_antialias(ANTIALIAS_BEST)
+  var xcoord, ycoord: float
+  let sf = image_surface_create(FORMAT_ARGB32, chart.size.w.cint, chart.size.h.cint)
   defer: surface_destroy(sf)
   result = create(sf)
+  #result.set_font_options(cfo)
   # chart frame
   if chart.frame.ltype != None:
     result.set_source_rgb(chart.frame.color.r, chart.frame.color.g, chart.frame.color.b)
@@ -177,32 +183,34 @@ proc plot*(chart: var Chart): ptr cairo_t =
     result.set_source_rgb(chart.title.font.fg.r, chart.title.font.fg.g, chart.title.font.fg.b)
     result.select_font_face(chart.title.font.name, chart.title.font.slant, chart.title.font.weight)
     result.set_font_size(chart.title.font.size)
-    result.text_extents(chart.title.text.cstring, te)
-    xcoord = 0
+    result.text_extents(chart.title.text.cstring, te.addr)
+    chart.title.size.h = te.height
+    chart.title.size.w = te.width
+    xcoord = (chart.size.w - chart.title.size.w) / 2.0
     case chart.title.loc:
-      of "north":
-        ycoord = chart.frame.thick + chart.gap + chart.title.font.size
-      of "south":
+      of NORTH:
+        ycoord = chart.frame.thick + chart.gap + chart.title.size.h
+      of SOUTH:
         ycoord = chart.size.h - chart.frame.thick - chart.gap
       else: discard
     result.move_to(xcoord, ycoord)
     result.show_text(chart.title.text.cstring)
+  #result.set_font_options(cfo)
   # lines
-  extrema(chart.series)
-  for s in chart.series:
-    if s.line.ltype == None:
-      continue
-    result.set_source_rgb(s.line.color.r, s.line.color.g, s.line.color.b)
-    result.set_line_width(s.line.thick)
-    # TODO assert len(x) == len(y)
-    for i in 0..len(s.x)-2:
-      result.move_to(chart.size.w.float*(s.x[i]-s.xmin)/(s.xmax-s.xmin), chart.size.h.float - chart.title.h - chart.size.h.float*(s.y[i]-s.ymin)/(s.ymax-s.ymin))
-      result.line_to(chart.size.w.float*(s.x[i+1]-s.xmin)/(s.xmax-s.xmin), chart.size.h.float - chart.title.h - chart.size.h.float*(s.y[i+1]-s.ymin)/(s.ymax-s.ymin))
-      result.stroke()
+  #extrema(chart.series)
+  #for s in chart.series:
+  #  if s.line.ltype == None:
+  #    continue
+  #  result.set_source_rgb(s.line.color.r, s.line.color.g, s.line.color.b)
+  #  result.set_line_width(s.line.thick)
+  #  # TODO assert len(x) == len(y)
+  #  for i in 0..len(s.x)-2:
+  #    result.move_to(chart.size.w*(s.x[i]-s.xmin)/(s.xmax-s.xmin), chart.size.h - chart.title.h - chart.size.h*(s.y[i]-s.ymin)/(s.ymax-s.ymin))
+  #    result.line_to(chart.size.w*(s.x[i+1]-s.xmin)/(s.xmax-s.xmin), chart.size.h - chart.title.h - chart.size.h*(s.y[i+1]-s.ymin)/(s.ymax-s.ymin))
+  #    result.stroke()
 
 proc writePNG*(cr: ptr cairo_t; png:string) =
   discard surface_write_to_png(get_target(cr), png)
-
 
 proc main() =
   var
@@ -214,6 +222,7 @@ proc main() =
   ds.x = x
   ds.y = y
   scatter.series.add(ds)
+  scatter.title.text = "CHART TITLE"
   scatter.plot().writePNG("test1.png")
 
 when isMainModule:
